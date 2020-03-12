8c5443632ca75374038c7c15f64fa71b9d816868
Nikolas Cane <n.kamysh@mosst.com>
2020-03-12 13:35:59 +0200

    add files

---
+++ Sources/RemoteImageView/RemoteImageFetcher.swift
@@ -0,0 +1,28 @@
+import SwiftUI
+
+public class RemoteImageFetcher: ObservableObject {
+  @Published var imageData = Data()
+  let url: URL
+  
+  public init(url: URL) {
+    self.url = url
+  }
+  
+  public func fetch() {
+    URLSession.shared.dataTask(with: self.url){(data, _, _) in
+      guard let data = data else { return }
+      DispatchQueue.main.async {
+        self.imageData = data
+      }
+    }
+    .resume()
+  }
+  
+  public func getImageData() -> Data {
+    return self.imageData
+  }
+  
+  public func getUrl() -> URL {
+    return self.url
+  }
+}
--- Sources/RemoteImageView/RemoteImageView.swift
+++ Sources/RemoteImageView/RemoteImageView.swift
@@ -1,3 +1,47 @@
-struct RemoteImageView {
-    var text = "Hello, World!"
+import SwiftUI
+
+public struct RemoteImageView<Content: View>: View {
+  @ObservedObject var imageFetcher: RemoteImageFetcher
+  var content: (_ image: Image) -> Content
+  let placeholder: Image
+  
+  @State var previousURL: URL? = nil
+  @State var imageData = Data()
+  
+  public init(placeholder: Image,
+              imageFetcher: RemoteImageFetcher,
+              content: @escaping (_ image: Image) -> Content ) {
+    self.placeholder = placeholder
+    self.imageFetcher = imageFetcher
+    self.content = content
+  }
+  
+  public var body: some View {
+    DispatchQueue.main.async {
+      if self.previousURL != self.imageFetcher.getUrl() {
+        self.previousURL = self.imageFetcher.getUrl()
+      }
+      if !self.imageFetcher.getImageData().isEmpty {
+        self.imageData = self.imageFetcher.getImageData()
+      }
+    }
+    var image: Image? = nil
+    if let uiImage = UIImage(data: self.imageData) {
+      image = Image(uiImage: uiImage)
+    }
+    
+    return ZStack {
+      if image != nil {
+        self.content(image!)
+      }
+      else {
+        self.content(placeholder)
+      }
+    }
+    .onAppear(perform: self.loadImage)
+  }
+  
+  private func loadImage() {
+    self.imageFetcher.fetch()
+  }
 }
